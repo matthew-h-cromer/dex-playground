83f2562bd52d15bce8eebce79814452d6a0a37a8
Matthew Cromer <matthew.h.cromer@gmail.com>
2022-05-06 11:49:59 -0700

    deploy TO-T1 LP when deploying uniswap

--- deploy/uniswap.js
+++ deploy/uniswap.js
@@ -1,9 +1,11 @@
+const { ethers } = require('hardhat');
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
   // WETH
-  const weth = await deploy('WETH', {
+  const wethDeployment = await deploy('WETH', {
     from: deployer,
     args: [],
     log: true,
@@ -11,7 +13,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
   // FACTORY
   const feeToSetter = deployer;
-  const factory = await deploy('UniswapV2Factory', {
+  const factoryDeployment = await deploy('UniswapV2Factory', {
     from: deployer,
     args: [feeToSetter],
     log: true,
@@ -20,8 +22,16 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   // ROUTER
   await deploy('UniswapV2Router02', {
     from: deployer,
-    args: [factory.address, weth.address],
+    args: [factoryDeployment.address, wethDeployment.address],
     log: true,
   });
+
+  // initialize LP
+  const token0 = await ethers.getContract('Token0');
+  const token1 = await ethers.getContract('Token1');
+  const factory = await ethers.getContract('UniswapV2Factory');
+  await factory.createPair(token0.address, token1.address);
+  const pair = await factory.getPair(token0.address, token1.address);
+  console.log(`T0-T1 LP deployed at ${pair}`);
 };
 module.exports.tags = ['Uniswap'];
