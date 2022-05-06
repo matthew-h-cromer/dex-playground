7db2b0665b33d4c172de2d5f36120d46ff91c516
Matthew Cromer <matthew.h.cromer@gmail.com>
2022-05-06 11:48:24 -0700

    remove unused vars

--- deploy/token0.js
+++ deploy/token0.js
@@ -1,10 +1,8 @@
-const { ethers } = require('hardhat');
-
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  const token0 = await deploy('Token0', {
+  await deploy('Token0', {
     from: deployer,
     args: [],
     log: true,
--- deploy/token1.js
+++ deploy/token1.js
@@ -2,7 +2,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  const token1 = await deploy('Token1', {
+  await deploy('Token1', {
     from: deployer,
     args: [],
     log: true,
